457a6f76df3b91e1aaaf9055c120f302931d3e06
ElliotRedhead <elliot.redhead@gmail.com>
2021-03-22 21:56:34 +0000

    Navigate dict to articles.

--- scraper.py
+++ scraper.py
@@ -11,18 +11,21 @@ target_page = 'https://www.binance.com/en/support/announcement/c-48?navId=48'
 page_response = urlopen(target_page)
 
 # Open output file
-f = open(sys.argv[1]+".json" or "output.json", "w+")
+if sys.argv[1:]:
+    file_output_name = sys.argv[1]
+else:
+    file_output_name = "output"
+# f = open(file_output_name+".json", "w+")
 
 # Parse the HTML using BeautifulSoup
 parsed_html = BeautifulSoup(page_response, "html.parser")
 target_tag = parsed_html.find(
     "script", type="application/json", id="__APP_DATA")
-print(target_tag)
 target_json = json.loads(target_tag.string)
 
-f.write(target_tag.string)
-print(target_json)
+# f.write(target_tag.string)
+print(target_json["routeProps"]["b723"]["navDataResource"][0]["articles"][0])
 # for line in parsed_html:
 #     f.write(line)
 
-f.close()
+# f.close()
