351ae156af212c127c01237862589a05e9c3d07a
ElliotRedhead <elliot.redhead@gmail.com>
2021-03-23 08:35:51 +0000

    Modularise workflow.

--- scraper.py
+++ scraper.py
@@ -4,28 +4,33 @@ import sys
 import re
 import json
 
-# Specify target URL
-target_page = 'https://www.binance.com/en/support/announcement/c-48?navId=48'
+# # Open output file
+# if sys.argv[1:]:
+#     file_output_name = sys.argv[1]
+# else:
+#     file_output_name = "output"
+# # f = open(file_output_name+".json", "w+")
 
-# Query the website and return the HTML
-page_response = urlopen(target_page)
+def scrape_page(url):
+    # Query the website and return the HTML
+    page_response = urlopen(url)
+    # Parse the HTML using BeautifulSoup
+    parsed_html = BeautifulSoup(page_response, "html.parser")
+    # Find specific script tag
+    target_tag = parsed_html.find("script", type="application/json", id="__APP_DATA")
+    target_json = json.loads(target_tag.string)
 
-# Open output file
-if sys.argv[1:]:
-    file_output_name = sys.argv[1]
-else:
-    file_output_name = "output"
-# f = open(file_output_name+".json", "w+")
+    return target_json
 
-# Parse the HTML using BeautifulSoup
-parsed_html = BeautifulSoup(page_response, "html.parser")
-target_tag = parsed_html.find(
-    "script", type="application/json", id="__APP_DATA")
-target_json = json.loads(target_tag.string)
 
-# f.write(target_tag.string)
-print(target_json["routeProps"]["b723"]["navDataResource"][0]["articles"][0])
-# for line in parsed_html:
-#     f.write(line)
 
-# f.close()
+
+def get_target_dict(parsed_json):
+    print(parsed_json["routeProps"]["b723"]["navDataResource"][0]["articles"][0])
+
+
+
+
+if __name__ == "__main__":
+    parsed_json = scrape_page("https://www.binance.com/en/support/announcement/c-48?navId=48")
+    target_dict = get_target_dict(parsed_json)
