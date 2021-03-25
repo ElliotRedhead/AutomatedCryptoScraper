13da25f973d2abd9b5823f6991686186e12ca6cc
ElliotRedhead <elliot.redhead@gmail.com>
2021-03-25 21:53:21 +0000

    Send email using env variables and args.

--- send_email.py
+++ send_email.py
@@ -1,14 +1,29 @@
 import requests
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
+mailgun_api=os.environ['MAILGUN_API']
+mailgun_domain=os.environ['MAILGUN_DOMAIN']
 
-def send_email():
+#
+def send_email(recipient_addresses):
+    """Send an email to target email addresses.
+
+    Parameters:
+    recipient_addresses (list): Target email address strings.
+
+    Returns:
+    Response code.
+
+    """
     return requests.post(
-        "https://api.mailgun.net/v3/domain/messages",
-        auth=("api", "apikey"),
-        data={"from": "Sender <mailgun@domain>",
-              "to": ["recipient@domain.com"],
+        f"https://api.mailgun.net/v3/{mailgun_domain}/messages",
+        auth=("api", mailgun_api),
+        data={"from": f"Sender <elliotn@{mailgun_domain}>",
+              "to": recipient_addresses,
               "subject": "Hello",
               "text": "Testing some Mailgun awesomness!"})
 
 
-send_email()
+print(send_email(["test@domain.com", "test2@domain.com"]))
