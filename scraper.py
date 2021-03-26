a00d3a7b2bebbdb1de1617d128e4a6f4c6ee0d43
elliot.redhead <elliot.redhead@gmail.com>
2021-03-26 23:28:40 +0000

    Pass target webpage to scrape from env.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,13 @@
+import os
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 import json
+from dotenv import load_dotenv
+load_dotenv()
+
+target_website = os.environ["TARGET_WEBPAGE"]
+
 
 def scrape_page(url):
     # Query the website and return the HTML
@@ -10,19 +16,19 @@ def scrape_page(url):
         # Parse the HTML using BeautifulSoup
         parsed_html = BeautifulSoup(page_response, "html.parser")
         # Find specific script tag
-        target_tag = parsed_html.find("script", type="application/json", id="__APP_DATA")
+        target_tag = parsed_html.find(
+            "script", type="application/json", id="__APP_DATA")
         target_json = json.loads(target_tag.string)
     except:
         print("A scraping error occurred, no results were produced.")
         # Log the error to a table
     return target_json
 
+
 def get_target_dict(parsed_json):
-    print(parsed_json["routeProps"]["b723"]["navDataResource"][0]["articles"][0])
-
-
+    return(parsed_json["routeProps"]["b723"]["navDataResource"][0]["articles"][0])
 
 
 if __name__ == "__main__":
-    parsed_json = scrape_page("https://www.binance.com/en/support/announcement/c-48?navId=48")
+    parsed_json = scrape_page(target_webpage)
     target_dict = get_target_dict(parsed_json)
