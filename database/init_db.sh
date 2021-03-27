7b72ce478cac3f8c081decbb5d1d7ce185b5aecf
elliot.redhead <elliot.redhead@gmail.com>
2021-03-27 14:05:39 +0000

    Use folder containing script as arg.

--- database/init_db.sh
+++ database/init_db.sh
@@ -1,2 +1,3 @@
-docker build . -t binancepostgres &&
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+docker build $DIR -t binancepostgres &&
 docker run --rm -P -d -p 5432:5432 --name binancepostgres binancepostgres
