3235776dce99d1e1656c8e2f1b553d0e8c067847
ElliotRedhead <elliot.redhead@gmail.com>
2021-03-28 20:48:27 +0100

    Stop (/remove) old docker container on build.

--- database/init_db.sh
+++ database/init_db.sh
@@ -1,3 +1,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+if [ "$(docker ps -q -f name=binancepostgres)" ]; then
+	# cleanup old container
+    docker stop binancepostgres
+fi
+
 docker build $DIR -t binancepostgres &&
 docker run --rm -P -d -p 5432:5432 --name binancepostgres binancepostgres
