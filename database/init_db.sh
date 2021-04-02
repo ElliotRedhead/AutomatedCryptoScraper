7d35b47e38edc29d0c30f7d9ae2f4009ccb3b543
elliot.redhead <elliot.redhead@gmail.com>
2021-04-02 20:33:39 +0100

    Functional init script.

--- database/init_db.sh
+++ database/init_db.sh
@@ -3,15 +3,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 if [ "$(docker ps -q -f name=binancepostgres)" ]; then
 	# cleanup old container
 	while true; do
-		printf "A binancepostgres container already exists, this process will overwrite the previous container. \n Do you wish to proceed? y/n \n"
+		echo "A binancepostgres container already exists, this process will overwrite the previous container."
+		read -p "Do you wish to proceed? (y/n) " yn
 		# select yn in "Yes" "No"; do
 		case $yn in
-			[Yy]* ) docker stop binancepostgres; break;;
+			[Yy]* ) docker stop binancepostgres && docker build $DIR -t binancepostgres && docker run --rm -P -d -p 5432:5432 --name binancepostgres binancepostgres; break;;
 			[Nn]* ) exit;;
-			* ) echo "Please answer y/yes or n/no."
+			* ) echo "Please answer y/yes or n/no. "
 		esac
-		# done
-	fi
-
-docker build $DIR -t binancepostgres &&
-docker run --rm -P -d -p 5432:5432 --name binancepostgres binancepostgres
+	done
+	else
+		docker build $DIR -t binancepostgres &&
+		docker run --rm -P -d -p 5432:5432 --name binancepostgres binancepostgres
+fi
