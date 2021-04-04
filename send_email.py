9618c557dd0e365d2215b3a094cb1d2e9eaa6070
ElliotRedhead <elliot.redhead@gmail.com>
2021-04-04 03:40:22 +0100

    Construct helper function, args restructure.

--- send_email.py
+++ send_email.py
@@ -7,7 +7,9 @@ mailgun_api = os.environ["MAILGUN_API"]
 mailgun_domain = os.environ["MAILGUN_DOMAIN"]
 
 
-def send_email(recipient_addresses):
+# Currently emails go to spam, and using a sandbox domain should not be used in production.
+
+def send_email(recipient_addresses, subject, text):
     """Send an email to target email addresses.
 
     Parameters:
@@ -17,13 +19,24 @@ def send_email(recipient_addresses):
     Response code.
 
     """
+    auth = ("api", mailgun_api)
+    data = {
+        "from": f"Automated Binance Announcement <mailgun@{mailgun_domain}>",
+        "to": recipient_addresses,
+        "subject": subject,
+        "text": text
+        }
     return requests.post(
         f"https://api.mailgun.net/v3/{mailgun_domain}/messages",
-        auth=("api", mailgun_api),
-        data={"from": f"Sender <elliotn@{mailgun_domain}>",
-              "to": recipient_addresses,
-              "subject": "Hello",
-              "text": "Testing some Mailgun awesomness!"})
+        auth=auth,
+        data=data
+        )
 
+def construct_coin_announcement(coin, users_dicts):
+    recipient_addresses = []
+    for user_dict in users_dicts:
+        recipient_addresses.append(user_dict['email'])
+    subject = f"Binance: {coin} Coin Innovation Zone Listing"
+    text = f"A coin ({coin}) has been announced for release on the Binance Innovation zone. \n View the new crypto listings news here: https://www.binance.com/en/support/announcement/c-48?navId=48"
 
-print(send_email(["test@domain.com", "test2@domain.com"]))
+    send_email(recipient_addresses, subject, text)
